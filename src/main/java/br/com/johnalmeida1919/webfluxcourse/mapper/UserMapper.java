417db1e6983f8940699552c381130ffde4b6576d
joaoalmeida1919 <joaoalmeida199@gmail.com>
2023-08-19 19:32:22 -0300

    add User Mapper

--- src/main/java/br/com/johnalmeida1919/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/johnalmeida1919/webfluxcourse/mapper/UserMapper.java
@@ -4,9 +4,13 @@ import br.com.johnalmeida1919.webfluxcourse.entity.User;
 import br.com.johnalmeida1919.webfluxcourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
 )
 public interface UserMapper {
     @Mapping(target = "id", ignore = true)
