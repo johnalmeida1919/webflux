21991ff054362f7ffe6fb69f4e55d00e6c945a12
joaoalmeida1919 <joaoalmeida199@gmail.com>
2023-08-30 19:16:14 -0300

    add impl to update user contract

--- src/main/java/br/com/johnalmeida1919/webfluxcourse/controller/impl/UserControllerImpl.java
+++ src/main/java/br/com/johnalmeida1919/webfluxcourse/controller/impl/UserControllerImpl.java
@@ -43,11 +43,13 @@ public class UserControllerImpl implements UserController {
 
     @Override
     public ResponseEntity<Mono<UserResponse>> update(String id, UserRequest request) {
-        return null;
+        return ResponseEntity.ok().body(userService.update(id, request).map(mapper::toResponse));
     }
 
     @Override
     public ResponseEntity<Mono<Void>> delete(String id) {
         return null;
     }
+
+
 }
--- src/main/java/br/com/johnalmeida1919/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/br/com/johnalmeida1919/webfluxcourse/mapper/UserMapper.java
@@ -3,10 +3,7 @@ package br.com.johnalmeida1919.webfluxcourse.mapper;
 import br.com.johnalmeida1919.webfluxcourse.entity.User;
 import br.com.johnalmeida1919.webfluxcourse.model.request.UserRequest;
 import br.com.johnalmeida1919.webfluxcourse.model.response.UserResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
@@ -17,4 +14,7 @@ public interface UserMapper {
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest request);
     UserResponse toResponse(final User entity);
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request, @MappingTarget final User entity);
 }
